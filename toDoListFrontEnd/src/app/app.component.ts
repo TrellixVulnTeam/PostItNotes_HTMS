671614ea6ba33f1a6a8a90c0c7a02d743e528fbf
Vasilis Kalogirou <kalogirou.va@gmail.com>
2022-02-21 20:09:11 +0200

    added front end services

--- toDoListFrontEnd/src/app/app.component.ts
+++ toDoListFrontEnd/src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Note } from './note';
+import { NoteService } from './note.service';
+import { Room } from './room';
+import { RoomService } from './room.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'toDoListFrontEnd';
+export class AppComponent implements OnInit {
+  public rooms!: Room[];
+  public notes!: Note[];
+
+  constructor(private noteService: NoteService,private roomService: RoomService){}
+
+  ngOnInit(){
+
+    this.getRooms();
+    this.getNotes();
+  }
+    public getNotes(): void{
+      this.noteService.getNotes().subscribe(
+        (response: Note[]) =>{
+          this.notes = response;
+        },
+        (error: HttpErrorResponse) => {
+          alert(error.message);
+        }
+      );
+    }
+
+    public getRooms(): void{
+      this.roomService.getRooms().subscribe(
+        (response: Room[]) =>{
+          this.rooms = response;
+        },
+        (error: HttpErrorResponse) => {
+          alert(error.message);
+        }
+      );
+    }
+
+
 }
---
+++ toDoListFrontEnd/src/app/note.ts
@@ -0,0 +1,7 @@
+import { Room } from './room';
+export interface Note {
+  id: number;
+  text: string;
+  color: string;
+  roomId: Room;
+}
